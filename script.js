c2845cbfd702a776349b38dc481f44ee87d21344
Victor Kwasara <victor@DESKTOP-87DPNVK.net>
2026-04-24 21:00:30 +0200

    particles update

--- script.js
+++ script.js
@@ -16,25 +16,32 @@ const mouse = {
   y: undefined,
 }
 
+let hue = 0;
+let sat = 100;
+let light = 45;
+
 canvas.addEventListener('click', (event) => {
   mouse.x = event.x;
   mouse.y = event.y;
+  for (let i = 0; i < 10; i++) {
+    particles.push(new Particle())
+  }
 }
 )
 canvas.addEventListener('mousemove', (event) => {
   mouse.x = event.x;
   mouse.y = event.y;
+  for (let i = 0; i < 5; i++) {
+    particles.push(new Particle())
+  }
 })
 
-function drawCircle() {
-
-}
 
 class Particle {
   constructor() {
-    this.x = Math.random() * canvas.width;
-    this.y = Math.random() * canvas.height;
-    this.size = Math.random() * 5 + 1;
+    this.x = mouse.x;
+    this.y = mouse.y;
+    this.size = Math.random() * 10 + 1;
     this.speedX = Math.random() * 3 - 1.5;
     this.speedY = Math.random() * 3 - 1.5
   }
@@ -42,36 +49,45 @@ class Particle {
   update() {
     this.x += this.speedX;
     this.y += this.speedY;
-
+    if (this.size > 0.2) this.size -= 0.01;
   }
 
   draw() {
-    ctx.fillStyle = 'blue';
+    ctx.fillStyle = `hsl(${hue}, ${sat}%, ${light}%)`;
     ctx.beginPath();
-    ctx.arc(this.x, this.y, 50, 0, Math.PI * 2);
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
     ctx.fill();
 
   }
 }
 
-function init() {
-  for (let i = 0; i < 100; i++) {
-    particles.push(new Particle())
-  }
+function adjustColor() {
+  hue = (hue + 1) % 360
+  sat = Math.floor(Math.random() * 100 + 1)
+}
+
+function updateEnvironment() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  adjustColor()
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.02)'
+  ctx.fillRect(0, 0, canvas.width, canvas.height)
 }
 
 function handleParticles() {
   for (let particle of particles) {
+
     particle.update();
     particle.draw();
+    if (particle.size <= 0.3) {
+      let i = particles.indexOf(particle)
+
+      particles.splice(i, 1);
+    }
   }
 }
 
-init()
-console.log(particles)
-
 function animate() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  updateEnvironment()
   handleParticles()
   requestAnimationFrame(animate);
 }
