c8263359eb1de3d9b80d857ac7e3bd3309b0fe9d
Victor Kwasara <victor@DESKTOP-87DPNVK.net>
2026-04-25 22:01:29 +0200

    feat: add lines to join nodes

--- script.js
+++ script.js
@@ -18,20 +18,20 @@ const mouse = {
 
 let hue = 0;
 let sat = 100;
-let light = 45;
+let light = 50;
 
-canvas.addEventListener('click', (event) => {
-  mouse.x = event.x;
-  mouse.y = event.y;
-  for (let i = 0; i < 10; i++) {
-    particles.push(new Particle())
-  }
-}
-)
+// canvas.addEventListener('click', (event) => {
+//   mouse.x = event.x;
+//   mouse.y = event.y;
+//   for (let i = 0; i < 10; i++) {
+//     particles.push(new Particle())
+//   }
+// }
+// )
 canvas.addEventListener('mousemove', (event) => {
   mouse.x = event.x;
   mouse.y = event.y;
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 2; i++) {
     particles.push(new Particle())
   }
 })
@@ -41,19 +41,20 @@ class Particle {
   constructor() {
     this.x = mouse.x;
     this.y = mouse.y;
+    this.color = `hsl(${hue}, ${sat}%, ${light}%)`;
     this.size = Math.random() * 10 + 1;
     this.speedX = Math.random() * 3 - 1.5;
-    this.speedY = Math.random() * 3 - 1.5
+    this.speedY = Math.random() * 10 - 1.5
   }
 
   update() {
     this.x += this.speedX;
     this.y += this.speedY;
-    if (this.size > 0.2) this.size -= 0.01;
+    if (this.size > 0.2) this.size -= 0.1;
   }
 
   draw() {
-    ctx.fillStyle = `hsl(${hue}, ${sat}%, ${light}%)`;
+    ctx.fillStyle = this.color;
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
     ctx.fill();
@@ -63,7 +64,6 @@ class Particle {
 
 function adjustColor() {
   hue = (hue + 1) % 360
-  sat = Math.floor(Math.random() * 100 + 1)
 }
 
 function updateEnvironment() {
@@ -74,14 +74,30 @@ function updateEnvironment() {
 }
 
 function handleParticles() {
-  for (let particle of particles) {
+  for (let i = 0; i < particles.length; i++) {
+    particles[i].update();
+    particles[i].draw();
 
-    particle.update();
-    particle.draw();
-    if (particle.size <= 0.3) {
-      let i = particles.indexOf(particle)
-
+    if (particles[i].size <= 1 || particles[i].x < 0 || particles[i].y < 0) {
       particles.splice(i, 1);
+      console.log(particles.length)
+      i--;
+      continue
+    }
+    for (let j = i; j < particles.length; j++) {
+      const dx = particles[i].x - particles[j].x
+      const dy = particles[i].y - particles[j].y
+
+      const distance = Math.sqrt(dx * dx + dy * dy);
+      console.log(distance)
+      if (distance <= 100) {
+        ctx.beginPath();
+        ctx.strokeStyle = particles[i].color;
+        ctx.moveTo(particles[i].x, particles[i].y)
+
+        ctx.lineTo(particles[j].x, particles[j].y)
+        ctx.stroke()
+      }
     }
   }
 }
